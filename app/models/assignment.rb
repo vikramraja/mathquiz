a44f73b9c8cdd5476590da53a22f883de3c3452c
Vikram Rajagoplan <vikram.raja@gmail.com>
2014-08-04 21:02:09 +0200

    almost done hooking up all of these models

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,5 +2,6 @@ class Assignment < ActiveRecord::Base
 
 	belongs_to :course
 	has_many :quizzes
+	has_one :topic
 
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,5 @@ class Quiz < ActiveRecord::Base
 	belongs_to :creator, class_name: :user
 	belongs_to :challenger, class_name: :user
 	belongs_to :assignment
+	has_one :topic, :through => :assignment
 end
