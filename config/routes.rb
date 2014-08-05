c6ff5a9b685b105ce79d4c8159e0f4b0d9f163d9
Liana <lkadisha@stanford.edu>
2014-08-05 21:22:41 +0200

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   resources :courses
 
   resources :assignments
-
-  root 'courses#new'
+  # resources :home, :only => [:index]
+  root 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
