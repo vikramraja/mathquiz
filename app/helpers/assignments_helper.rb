e0ca11c00c9e4c04d8903470f1ce37e0b9742b9a
Vikram Rajagoplan <vikram.raja@gmail.com>
2014-08-07 12:55:15 +0200

    added an assignment helper and changed some small things in index

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,2 +1,35 @@
 module AssignmentsHelper
 end
+
+
+
+def findStudentQuizScore(assignment_id)
+	topscore = -1
+	Assignment.find(assignment_id).quizzes.each do |c|
+		if table_student_id == c.creator_id
+			unless topscore > c.creator_score
+				topscore = c.creator_score
+			end
+		elsif table_student_id == c.challenger_id
+			unless topscore > c.challenger_score
+				topscore = c.challenger_score
+			end
+		end
+
+		if topscore == -1 
+			return "Not Started"
+		else
+			return topscore.to_s
+		end
+
+
+
+
+
+
+
+
+
+
+end
+
