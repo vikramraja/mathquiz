2e9f7b11818a7b493c4a346ed7949ec78f17e826
Liana <lkadisha@stanford.edu>
2014-08-07 15:09:37 +0200

    added alert if student email already enrolled

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,11 +2,15 @@ class StudentsController < ApplicationController
 	
 
 	def create
-		@course= Course.find params[:course_id]
-		student = User.invite!(user_attributes)
-		binding.pry
-		@course.users << student
-		redirect_to assignments_path
+		if User.find_by(:email => user_attributes[:email])
+			flash[:alert] = "This student is already enrolled in a course."
+			redirect_to assignments_path
+		else
+			@course= Course.find params[:course_id]
+			student = User.invite!(user_attributes)
+			@course.users << student
+			redirect_to assignments_path
+		end
 	end
 
 	private
