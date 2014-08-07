a1f6169976da8202462ab2e4ce968a46c3157d9b
Liana <lkadisha@stanford.edu>
2014-08-07 14:26:23 +0200

    add flash alert message

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,7 @@ class StudentsController < ApplicationController
 	def create
 		@course= Course.find params[:course_id]
 		student = User.invite!(user_attributes)
+		binding.pry
 		@course.users << student
 		redirect_to assignments_path
 	end
