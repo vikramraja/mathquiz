3d245f6fae6d36eb62712062e9a00f025b0562c0
Vikram Rajagoplan <vikram.raja@gmail.com>
2014-08-07 15:57:49 +0200

    created basic quiz implementation

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,2 +1,24 @@
 module QuizzesHelper
+
+	def operandifier(operand) #outputs a string
+		word =operand.downcase 
+		if word == "addition"
+			return "+"
+
+		elsif word == "subtraction"
+			return "-"
+
+		elsif word == "multiplication"
+			return "*"
+		end
+	end
+
+	def display_problem(item1, item2, operand)
+		return item1.to_s + " " + operand+ " " + item2.to_s
+
+
+			
+
+
+
 end
