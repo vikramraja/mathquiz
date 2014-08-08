647f0376bdca4282291f99a93880708db3ad32f8
Vikram Rajagoplan <vikram.raja@gmail.com>
2014-08-08 11:37:53 +0200

    im tired of writing commit messages

--- app/models/course.rb
+++ app/models/course.rb
@@ -11,6 +11,6 @@
 
 class Course < ActiveRecord::Base
 	has_many :enrollments, :dependent => :destroy
-	has_many :users, :through => :enrollments
+	has_many :users, :through => :enrollments, :dependent => :destroy
 	has_many :assignments, :dependent => :destroy
 end
